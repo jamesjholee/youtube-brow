8bb6119d7f7aa2836564c283c0ed4696163a8645
James <james.lee4789@gmail.com>
2019-06-20 21:29:45 -0400

    remove warnings

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,11 +10,10 @@ const VideoDetail = ({video}) => {
   return (
     <div>
       <div className="ui embed">
-      <iframe src={videoSrc} />
+      <iframe title="video-player" src={videoSrc} />
       </div>
 
       <div className="ui segment">
-
         <h4 className="ui header">{video.snippet.title}</h4>
         <p className="">{video.snippet.description}</p>
       </div>
