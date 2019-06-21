c9528c4c0bc760f21d232e6ff530dbc21ef54d6a
James <james.lee4789@gmail.com>
2019-06-20 23:44:26 -0400

    finish mvp

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({videos, onVideoSelect}) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem key={video.id.video.Id} video={video} onVideoSelect={onVideoSelect} />
+    return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
   });
   return <div className='ui relaxed divided list'>{renderedList}</div>;
 }
